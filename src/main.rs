c1d4496c056c4101581e1f9533aef2ed319de22a
J/A <archer884@zoho.com>
2021-04-26 18:15:00 -0500

    Cleanup nonsense

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,36 @@
-use std::{
-    fmt::Display,
-    io::{self, Read},
-};
+use std::io::{self, Read};
 
-use clap::Clap;
-use either::Either;
 use hashbrown::HashSet;
-use regex::Regex;
+use linkex::LinkExtractor;
+use structopt::StructOpt;
 
-#[derive(Clap, Clone, Debug)]
+#[derive(Clone, Debug, structopt::StructOpt)]
 struct Opts {
+    source: Option<String>,
+    #[structopt(short, long)]
     base: Option<String>,
-    #[clap(short, long)]
+    #[structopt(short, long)]
     filter: Option<String>,
-    #[clap(short = 'a', long = "from-text")]
-    from_text: bool,
-}
-
-#[derive(Clone, Debug)]
-struct Extractor {
-    href_pattern: Regex,
-    url_pattern: Regex,
-    www_pattern: Regex,
-    from_text: bool,
-}
-
-impl Extractor {
-    fn new(from_text: bool) -> Self {
-        Self {
-            href_pattern: Regex::new(r#"href="([^"]+)""#).unwrap(),
-            url_pattern: Regex::new(r"(http|https)://[\w,./?'$%&*()+=-]+").unwrap(),
-            www_pattern: Regex::new(r#"www\.[\w,./?'$%&*()+=-]+"#).unwrap(),
-            from_text,
-        }
-    }
-
-    fn extract<'a>(&'a self, content: &'a str) -> impl Iterator<Item = &str> {
-        if self.from_text {
-            Either::Left(
-                self.href_pattern
-                    .captures_iter(content)
-                    .map(|x| x.get(1).unwrap().as_str())
-                    .chain(self.url_pattern.find_iter(content).map(|x| x.as_str()))
-                    .chain(self.www_pattern.find_iter(content).map(|x| x.as_str())),
-            )
-        } else {
-            Either::Right(
-                self.href_pattern
-                    .captures_iter(content)
-                    .map(|x| x.get(1).unwrap().as_str()),
-            )
-        }
-    }
-}
-
-impl Default for Extractor {
-    fn default() -> Self {
-        Self::new(false)
-    }
-}
-
-/// A struct used to make saints.
-#[derive(Clone, Debug, Default)]
-struct Canonizer {
-    base: Option<String>,
-}
-
-impl Canonizer {
-    /// Make a string into a saint.
-    fn canonize<'a, 'b>(&'b self, link: &'a str) -> Canonization<'a, 'b> {
-        if link.starts_with("http") {
-            return Canonization::Original(link);
-        }
-
-        if link.starts_with("www") {
-            return Canonization::AddHttp(link);
-        }
-
-        self.base
-            .as_ref()
-            .map(|base| Canonization::AddBase(link, base))
-            .unwrap_or_else(|| Canonization::Original(link))
-    }
-}
-
-enum Canonization<'a, 'b> {
-    AddBase(&'a str, &'b str),
-    AddHttp(&'a str),
-    Original(&'a str),
-}
-
-impl Display for Canonization<'_, '_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Canonization::AddBase(link, base) => write!(f, "{}{}", base, link),
-            Canonization::Original(link) => f.write_str(link),
-            Canonization::AddHttp(link) => write!(f, "http://{}", link),
-        }
-    }
 }
 
 fn main() {
     let Opts {
+        source,
         base,
         filter,
-        from_text,
-    } = Opts::parse();
+    } = Opts::from_args();
 
-    let extractor = Extractor::new(from_text);
-    let canonizer = Canonizer { base };
-    let content = read_stdin();
+    let content = match source {
+        Some(url) => read_url(&url),
+        None => read_stdin(),
+    };
 
-    // Tragically, we need to realize these strings ahead of time in order to
-    // filter duplicates.
-    let links: Vec<_> = extractor
-        .extract(&content)
-        .map(|x| canonizer.canonize(x).to_string())
-        .collect();
+    let extractor = base
+        .map(|base| LinkExtractor::with_base_url(base))
+        .unwrap_or_default();
 
+    // Evaluate canonization to check for duplicates.
+    let links: Vec<_> = extractor.extract(&content).map(|x| x.to_string()).collect();
     let mut duplicate_filter = HashSet::new();
     let unique_links = links
         .iter()
@@ -158,33 +71,6 @@ fn read_stdin() -> String {
     buf
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::Extractor;
-
-    #[test]
-    fn extractor_does_things() {
-        let extractor = Extractor::new(true);
-        let content = include_str!("../resources/input.html");
-        let actual: Vec<_> = extractor.extract(content).collect();
-        let expected = &[
-            "/topics/rust",
-            "/topics/infrastructure",
-            "/topics/search",
-            "/topics/index",
-            "/topics/graph",
-            "/topics/database",
-            "/topics/server",
-            "/topics/backend",
-            "/topics/search-server",
-            "http://foo.bar",
-            "https://www.bar.baz",
-            "http://foo.com/hello?name=Jack%20Ballenger&occupation=Barber",
-            "http://foo.com/with/pluses+for+spaces",
-            "http://foo.com/with/minuses-for-spaces",
-            "www.bar.baz",
-            "www.google.com",
-        ];
-        assert_eq!(actual, expected);
-    }
+fn read_url(url: &str) -> String {
+    ureq::get(url).call().unwrap().into_string().unwrap()
 }
