cdf355285db6aeeff76ec97838e07bb4ec57d24d
J/A <archer884@zoho.com>
2021-09-20 19:23:52 -0500

    Ok, there

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,42 @@
 use std::io::{self, Read};
 
+use clap::Clap;
 use hashbrown::HashSet;
 use linkex::LinkExtractor;
-use structopt::StructOpt;
 
-#[derive(Clone, Debug, structopt::StructOpt)]
+#[derive(Clap, Clone, Debug)]
 struct Opts {
-    source: Option<String>,
-    #[structopt(short, long)]
     base: Option<String>,
-    #[structopt(short, long)]
-    filter: Option<String>,
+    
+    /// source url
+    #[clap(short, long)]
+    source: Option<String>,
 }
 
 fn main() {
-    let Opts {
-        source,
-        base,
-        filter,
-    } = Opts::from_args();
+    let opts = Opts::parse();
+    if let Err(e) = run(&opts) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
 
-    let content = match source {
-        Some(url) => read_url(&url),
-        None => read_stdin(),
-    };
-
-    let extractor = base
-        .map(|base| LinkExtractor::with_base_url(base))
-        .unwrap_or_default();
+fn run(opts: &Opts) -> anyhow::Result<()> {
+    let extractor = opts.base.as_ref().map(LinkExtractor::with_base_url).unwrap_or_default();
+    let text = opts.source.as_ref()
+        .map(|url| reqwest::blocking::get(url).and_then(|cx| cx.text()))
+        .transpose()?
+        .unwrap_or_else(read_stdin);
 
     // Evaluate canonization to check for duplicates.
-    let links: Vec<_> = extractor.extract(&content).map(|x| x.to_string()).collect();
+    let links: Vec<_> = extractor.extract(&text).map(|x| x.to_string()).collect();
     let mut duplicate_filter = HashSet::new();
     let unique_links = links
         .iter()
-        .filter(|&link| duplicate_filter.insert(protocol_agnostic(link.as_ref())));
+        .filter(|&link| duplicate_filter.insert(strip_protocol(link.as_ref())));
 
-    match filter {
-        Some(filter) => display_filtered_links(&filter, unique_links),
-        None => display_links(unique_links),
-    }
+    display_links(unique_links);
+    Ok(())
 }
 
 fn display_links(links: impl IntoIterator<Item = impl AsRef<str>>) {
@@ -49,17 +46,7 @@ fn display_links(links: impl IntoIterator<Item = impl AsRef<str>>) {
     }
 }
 
-fn display_filtered_links(filter: &str, links: impl IntoIterator<Item = impl AsRef<str>>) {
-    for link in links {
-        let link = link.as_ref();
-        if link.contains(filter) {
-            println!("{}", link);
-        }
-    }
-}
-
-/// Strip the protocol from the left hand side of a url.
-fn protocol_agnostic(link: &str) -> &str {
+fn strip_protocol(link: &str) -> &str {
     link.find(':').map(|idx| &link[idx..]).unwrap_or(link)
 }
 
@@ -69,8 +56,4 @@ fn read_stdin() -> String {
     let mut buf = String::new();
     handle.read_to_string(&mut buf).unwrap();
     buf
-}
-
-fn read_url(url: &str) -> String {
-    ureq::get(url).call().unwrap().into_string().unwrap()
 }
